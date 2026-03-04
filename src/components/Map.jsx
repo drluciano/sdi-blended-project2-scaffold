cca0198f1629f01dbdfd5512b2c584b7af3aba7a
drluciano <dylanrivera@Dylans-MacBook-Pro.local>
2026-03-04 17:58:30 -0500

    added adding and removing map markers and polygons to localStorage

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,14 @@
 import 'leaflet/dist/leaflet.css';
-import {MapContainer, TileLayer} from 'react-leaflet';
+import {FeatureGroup, MapContainer, TileLayer} from 'react-leaflet';
 import L from 'leaflet';
 import icon from 'leaflet/dist/images/marker-icon.png';
 import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import '@geoman-io/leaflet-geoman-free';
+import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';
+import GeomanLayer from './GeomanLayer.jsx';
 
 const MapComponent = () => {
+
     let DefaultIcon = L.icon({
         iconUrl: icon,
         shadowUrl: iconShadow,
@@ -13,7 +17,7 @@ const MapComponent = () => {
     });
 
     L.Marker.prototype.options.icon = DefaultIcon;
-    
+
 
     return (
         <MapContainer center={[51.505, -0.09]} zoom={13}
@@ -22,6 +26,9 @@ const MapComponent = () => {
                 url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
+            <FeatureGroup>
+                <GeomanLayer/>
+            </FeatureGroup>
         </MapContainer>
     );
 };
